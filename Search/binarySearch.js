478d427ab785783a8bcb074ca2364d334738cfc2
Thomas <tholmes59@gmail.com>
2019-11-24 17:56:27 -0800

    add binary, linear and native string searches

---
+++ Search/binarySearch.js
@@ -0,0 +1,36 @@
+// Original Solution
+function binarySearch(arr, elem) {
+    var start = 0;
+    var end = arr.length - 1;
+    var middle = Math.floor((start + end) / 2);
+    while(arr[middle] !== elem && start <= end) {
+        if(elem < arr[middle]){
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+        middle = Math.floor((start + end) / 2);
+    }
+    if(arr[middle] === elem){
+        return middle;
+    }
+    return -1;
+}
+
+// Refactored Version
+function binarySearch(arr, elem) {
+    var start = 0;
+    var end = arr.length - 1;
+    var middle = Math.floor((start + end) / 2);
+    while(arr[middle] !== elem && start <= end) {
+        if(elem < arr[middle]) end = middle - 1;
+        else start = middle + 1;
+        middle = Math.floor((start + end) / 2);
+    }
+    return arr[middle] === elem ? middle : -1;
+}
+
+binarySearch([2,5,6,9,13,15,28,30], 103)
+
+// Big O
+// O(log N) (O(1) is best case if we hit on first try) - Doubling number of elem adds one more step
---
+++ Search/linearSearch.js
@@ -0,0 +1,11 @@
+function linearSearch(arr, val){
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] === val) return i;
+    }
+    return -1;
+}
+
+linearSearch([34,51,1,2,3,45,56,687], 100)
+
+// Big O
+// O(N) (O(1) best case but depends on the size of the arr)
